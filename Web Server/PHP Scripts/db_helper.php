fc8d9d5f83ff63f92d7901f26e3a18b2dbc3c5d4
Nicolas Papin <nmpapin@gmail.com>
2012-03-13 04:22:28 -0400

    Working on getting the data.

---
+++ Web Server/PHP Scripts/db_helper.php
@@ -0,0 +1,115 @@
+<?php
+	// Class used to handle database connections and work.
+	class Database
+	{
+		// Our connection.
+		var $connection;
+	
+		// Constructor
+		function __construct()
+		{
+			$db_host = "localhost";
+			$db_database = "nmpapin_aroute";
+			$db_username = "nmpapin_user";
+			$db_password = "user_password";
+		
+			// Initialize the connection.
+			$this->connection = mysql_connect($db_host, $db_username, $db_password);
+			if(!$this->connection)
+			{
+				die("An error occured while connecting to the database.<br /><br />" . mysql_error());
+			}
+			
+			// Select the database.
+			$db = mysql_select_db($db_database);
+			if(!$db)
+			{
+				die("An error occured while connecting to the database.<br /><br />" . mysql_error());
+			}
+			
+			// Take care of destroying the database on shutdown.
+			register_shutdown_function(array(&$this, "shutdown" ));
+		}
+		
+		// Obtains the result of a database select statement as an associated array.
+		function getResults($dbQuery)
+		{
+			$dbResults = mysql_query($dbQuery);
+			if(!$dbResults)
+			{
+				die(mysql_error());
+			}
+			
+			// Test for results.
+			$resultsArray = array();
+			if(mysql_num_rows($dbResults) > 0)
+			{
+				while($row = mysql_fetch_assoc($dbResults))
+				{
+					$resultsArray[] = $row;
+				}
+			}
+			else // If we do not have any results, return an empty array.
+			{
+				return array();
+			}
+			
+			return $resultsArray;
+		}
+		
+		// Obtains a single result from a database select statement.  This will always return the first result.
+		// null is returned in the case that nothing is found.
+		function getResult($dbQuery)
+		{
+			$dbResults = mysql_query($dbQuery);
+	
+			if(!$dbResults)
+			{
+				die(mysql_error());
+			}
+		
+			if(mysql_num_rows($dbResults) > 0)
+			{
+				return mysql_fetch_assoc($dbResults);
+			}
+			else
+			{
+				return null;
+			}
+		}
+		
+		// Runs an actionable query and returns the number of rows affected.
+		function getRowsAffected($dbQuery)
+		{
+			$dbResults = mysql_query($dbQuery);
+			if($dbResults)
+			{
+				return array('rowsAffected'=>mysql_affected_rows());
+			}
+			else
+			{
+				die(mysql_error());
+			}
+		}
+		
+		// Runs an insert query and returns the first inserted auto_increment id.
+		function getResultInserted($dbQuery)
+		{
+			$dbResults = mysql_query($dbQuery);
+			if($dbResults)
+			{
+				return mysql_insert_id();
+			}
+			else
+			{
+				die(mysql_error());
+			}
+		}
+		
+		// Closes the database connection.
+		function shutdown()
+		{
+			mysql_close($this->connection);
+		}
+	}
+?>
---
+++ Web Server/PHP Scripts/load_foreign_data_hacked_soln.php
@@ -0,0 +1,99 @@
+<?php
+	require_once 'db_helper.php';
+	
+	$database = new Database();
+	
+	function do_post_request($url, $data, $optional_headers = null)
+	{
+		$post_url = '';
+		foreach($data AS $key=>$value) 
+			$post_url .= $key.'='.$value.'&'; 
+		$post_url = rtrim($post_url, '&'); 
+
+		$params = array('http' => array(
+			'method' => 'POST',
+			'content' => $post_url
+		));
+		
+		if ($optional_headers !== null) 
+		{
+			$params['http']['header'] = $optional_headers;
+		}
+		
+		$ctx = stream_context_create($params);
+		
+		$fp = @fopen($url, 'rb', false, $ctx);
+		if (!$fp) 
+		{
+			throw new Exception("Problem with $url, $php_errormsg");
+		}
+		
+		$response = @stream_get_contents($fp);
+		if ($response === false) 
+		{
+			throw new Exception("Problem reading data from $url, $php_errormsg");
+		}
+		
+		fclose($fp);
+		
+		return $response;
+	}
+	
+	// Format Array of {"id": #, "short_name": bus_route or line, "name": name}
+	/*$bus_routes = json_decode(file_get_contents("http://spice.ridecell.com/get_routes/buses/"), true);
+	$train_routes = json_decode(file_get_contents("http://spice.ridecell.com/get_routes/trains/"), true);
+	
+	$route_id_map = array();
+	foreach($bus_routes AS $value)
+	{
+		$route_vals = "('".mysql_real_escape_string($value['short_name'])."','".mysql_real_escape_string($value['name'])."','Bus')";
+		
+		$route_id_map[$value['id']] = $database->getResultInserted
+		(
+			"INSERT INTO Route
+			(marta_id, route_name, type)
+			VALUES $route_vals;"
+		);
+	}
+	foreach($train_routes AS $value)
+	{
+		$route_vals = "('".mysql_real_escape_string($value['short_name'])."','".mysql_real_escape_string($value['name'])."','Train')";
+		
+		$route_id_map[$value['id']] = $database->getResultInserted
+		(
+			"INSERT INTO Route
+			(marta_id, route_name, type)
+			VALUES $route_vals;"
+		);
+	}
+	
+	$routes = array_merge($bus_routes, $train_routes);
+	
+	$headsign_route_map = array();*/
+	for($i = 96; $i < 97; $i++)
+	{	
+		$headsigns = json_decode(do_post_request("http://spice.ridecell.com/desktop/route_headsigns/", array("route_id" => $i)), true);
+		$headsigns = $headsigns[1];
+		$headsign_route_map[$i] = $headsigns;
+		
+		foreach($headsigns AS $val2)
+		{
+			$head_val = "(".mysql_real_escape_string($val2['direction']).",'".mysql_real_escape_string($val2['headsign'])."',".mysql_real_escape_string($i).")";
+			
+			$database->getResultInserted
+			(
+				"INSERT INTO Route_Variation
+				(direction, variation_name, route_id)
+				VALUES $head_val;"
+			);
+		}
+	}
+	
+	//$head_val = trim($head_val, ',');
+	
+	// Format: [success?, [{"direction": #(0 = south; 1 = north; 2 = east; 3 = west), "name": short_name, "color": color to print, "headsign": reads on bus, "route_id": #, "shape": array of 
+	// coords for map, shape_id: "#"}, ...], bounds, error message
+	//$headsigns = do_post_request("http://spice.ridecell.com/desktop/route_headsigns/", array("route_id" => $bus_routes[0]['id']));
+	
+	//echo $headsigns;
+?>
